dbded3b0d9fa1ebc44f1b6da8f10f174e01ecf59
David Oliveira <dedepco@gmail.com>
2022-05-23 17:19:41 -0300

    test: verify name while creating user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,11 +1,23 @@
 import { left } from '../shared'
 import { User } from './user'
-import { InvalidEmailError } from './errors'
+import { InvalidEmailError, InvalidNameError } from './errors'
 
 describe('User domain entity', () => {
   test('Should not create user with invalid e-mail address', () => {
     const invalidEmail = 'invalid_email'
     const error = User.create({ name: 'any_name', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError(invalidEmail)))
+  })
+
+  test('should not create user with invalid name (too few characters)', () => {
+    const invalidName = 'O             '
+    const error = User.create({ name: invalidName, email: 'any_email@mail.com' })
+    expect(error).toEqual(left(new InvalidNameError(invalidName)))
+  })
+
+  test('should not create user with invalid name (too many characters)', () => {
+    const invalidName = 'O'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'any_email@mail.com' })
+    expect(error).toEqual(left(new InvalidNameError(invalidName)))
   })
 })
