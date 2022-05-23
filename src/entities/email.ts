8aa361d33bede3cef9e0617589aa745282e0bca4
David Oliveira <dedepco@gmail.com>
2022-05-22 23:35:06 -0300

    feat: validate emails with domain parts larger than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -3,9 +3,14 @@ export class Email {
     if (!email) return false
 
     if (email.length > 320) return false
+
     const [local, domain] = email.split('@')
+
     if (local.length > 64 || local.length === 0) return false
     if (domain.length > 255 || domain.length === 0) return false
+
+    const domainParts = domain.split('.')
+    if (domainParts.some(part => part.length > 63)) return false
 
     return true
   }
