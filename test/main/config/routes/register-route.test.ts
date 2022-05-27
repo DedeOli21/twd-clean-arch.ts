c2dfd3b74ee5ce9b662b2a19f3f6a72465cc143c
David Oliveira <dedepco@gmail.com>
2022-05-27 00:15:05 -0300

    fix: connect to and disconnect from mongo

--- test/main/config/routes/register-route.test.ts
+++ test/main/config/routes/register-route.test.ts
@@ -1,19 +1,19 @@
 import request from 'supertest'
 import app from '@/main/config/app'
-// import { MongoHelper } from '@/external/repositories/mongodb/helper'
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
 describe('Register route', () => {
-  // beforeAll(async () => {
-  //   await MongoHelper.connect(process.env.MONGO_URL)
-  // })
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
 
-  // afterAll(async () => {
-  //   await MongoHelper.disconnect()
-  // })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
 
-  // beforeEach(async () => {
-  //   await MongoHelper.clearCollection('users')
-  // })
+  beforeEach(async () => {
+    await MongoHelper.clearCollection('users')
+  })
 
   test('should return an account on success', async () => {
     app.post('/test_cors', (req, res) => {
