9b1041dd8d4416a1ddb8325940f9517b28708a1c
David Oliveira <dedepco@gmail.com>
2022-05-27 00:03:43 -0300

    refactor: use mongodb user repository on register controller

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,16 +1,14 @@
 import { RegisterAndSendEmailController } from '@/web-controllers/'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list/'
-import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
-// // import { MongodbUserRepository } from '@/external/repositories/mongodb'
+import { MongodbUserRepository } from '@/external/repositories/mongodb'
 // // import { SendEmail } from '@/usecases/send-email'
 // // import { NodemailerEmailService } from '@/external/mail-services'
 // // import { getEmailOptions } from '@/main/config/email'
 // // import { RegisterAndSendEmail } from '@/usecases/register-and-send-email'
 
 export const makeRegisterAndSendEmailController = (): RegisterAndSendEmailController => {
-//   // const mongoDbUserRepository = new MongodbUserRepository()
-  const inMemoryUserRepository = new InMemoryUserRepository([])
-  const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(inMemoryUserRepository)
+  const mongoDbUserRepository = new MongodbUserRepository()
+  const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mongoDbUserRepository)
   const registerUserController = new RegisterAndSendEmailController(registerUserOnMailingListUseCase)
   return registerUserController
   // const registerAndSendEmailUseCase = new RegisterAndSendEmail(registerUserOnMailingListUseCase)
